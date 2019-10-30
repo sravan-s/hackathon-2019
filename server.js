accbb11be3b348182ff83a836cb0e67e487e23bd
ben sadick <bsadick@gmail.com>
2019-10-30 20:59:10 +0900

    url encoded

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const dotenv = require("dotenv");
 dotenv.config({path: "./config/config.env"});
 
 const db = require("./db");
+
 db();
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 app.get("/", (req, res)=>{
     res.send("Sending data")
 })
