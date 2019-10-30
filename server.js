7ce2e4c08169ebb33496ce8afbdaa53ae857c844
ben sadick <bsadick@gmail.com>
2019-10-30 21:12:53 +0900

    login added

--- server.js
+++ server.js
@@ -20,7 +20,10 @@ app.get("/index.html", (req, res)=>{
 
 app.post("/data", (req, res)=>{
     console.log(req.body)
-    res.end("data sent")
+    let {name, pass} = req.body;
+    res.status(200).json({
+        message: "Success!"
+    })
 })
 
 app.listen(5000, ()=>{
