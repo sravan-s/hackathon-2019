5fa197cd5f79ed51cb54ac68931f379013f9f237
ben sadick <bsadick@gmail.com>
2019-10-30 20:52:59 +0900

    server finished for step 1

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Email = require("./models/Email");
+const app = express();
 const dotenv = require("dotenv");
 
+dotenv.config({path: "./config/config.env"});
+
+const db = require("./db");
+db();
+app.get("/", (req, res)=>{
+    res.send("Sending data")
+})
+
+app.post("/data", (req, res)=>{
+    console.log(req.body)
+    res.end("data sent")
+})
+
+app.listen(5000, ()=>{
+    console.log("listening on 5000")
+})
